74c9699fb5bc12120ecfa30d665d8d6fa950b3bd
Suriyanad <106733060+Suriyanad@users.noreply.github.com>
2023-09-11 15:02:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,4 @@
 /*const video = document.getElementById("video");
-
-Promise.all([
-  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-]).then(startWebcam);
-
-function startWebcam() {
-  navigator.mediaDevices
-    .getUserMedia({
-      video: true,
-      audio: false,
-    })
-    .then((stream) => {
-      video.srcObject = stream;
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
-
-function getLabeledFaceDescriptions() {
-  const labels = ["suriya"];
-  return Promise.all(
-    labels.map(async (label) => {
-      const descriptions = [];
-      for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`./labels/${label}/${i}.jpg`);
-        const detections = await faceapi
-          .detectSingleFace(img)
-          .withFaceLandmarks()
-          .withFaceDescriptor();
-        descriptions.push(detections.descriptor);
-      }
-      return new faceapi.LabeledFaceDescriptors(label, descriptions);
-    })
-  );
-}
-
-video.addEventListener("play", async () => {
-  const labeledFaceDescriptors = await getLabeledFaceDescriptions();
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
-
-  const canvas = faceapi.createCanvasFromMedia(video);
-  document.body.append(canvas);
-
-  const displaySize = { width: video.width, height: video.height };
-  faceapi.matchDimensions(canvas, displaySize);
-
-  setInterval(async () => {
-    const detections = await faceapi
-      .detectAllFaces(video)
-      .withFaceLandmarks()
-      .withFaceDescriptors();
-
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-
-    const results = resizedDetections.map((d) => {
-      return faceMatcher.findBestMatch(d.descriptor);
-    });
-    results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box;
-      const drawBox = new faceapi.draw.DrawBox(box, {
-        label: result,
-      });
-      drawBox.draw(canvas);
-    });
-  }, 100);
-});
-*/
-const video = document.getElementById("video");
 // Function to get query parameters from the URL
 function getQueryParameter(parameterName) {
     const queryString = window.location.search;
@@ -194,6 +121,111 @@ video.addEventListener("play", async () => {
         isSuriyaDetected = true;
       }
 
+      drawBox.draw(canvas);
+    });
+  }, 100);
+});
+*/
+const video = document.getElementById("video");
+const recognitionWindow = window.open('https://comforting-hamster-925641.netlify.app/', '_blank');
+Promise.all([
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+
+    
+          
+            
+    
+
+          
+          Expand Down
+    
+    
+  
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+]).then(startWebcam);
+function startWebcam() {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: false,
+    })
+    .then((stream) => {
+      video.srcObject = stream;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+function getLabeledFaceDescriptions() {
+  const labels = ["suriya"];
+  return Promise.all(
+    labels.map(async (label) => {
+      const descriptions = [];
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`./labels/${label}/${i}.jpg`);
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detections.descriptor);
+      }
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+    })
+  );
+}
+video.addEventListener("play", async () => {
+  const labeledFaceDescriptors = await getLabeledFaceDescriptions();
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
+  let isSuriyaDetected = false; // Flag to track detection status
+  // Set a timeout of 10 seconds to send the result to Salesforce
+  setTimeout(async () => {
+    if (isSuriyaDetected) {
+      // Send a POST request to Salesforce indicating "suriya" is detected
+      const response = await fetch("https://dream-force-2814-dev-ed.scratch.my.salesforce.com/services/apexrest/Baytree/facerecognition", {
+        method: "POST",
+        body: { "detected": "true" },
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer 00D0p0000002ZU3!AQgAQIIyMbhb4245_z7CBMgAOWJHgALDqD95kN301sM6JZrx3U9yx3sW1R8eKWCTyCTL0wWjWEMvxfb6rNMj5C9Pt2ployTF"
+        },
+      });
+      console.log("Suriya detected:", response.status);
+    } else {
+      // Send a POST request to Salesforce indicating "suriya" is not detected
+      const response = await fetch("https://dream-force-2814-dev-ed.scratch.my.salesforce.com/services/apexrest/Baytree/facerecognition", {
+        method: "POST",
+        body: { "detected": "false" },
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer 00D0p0000002ZU3!AQgAQIIyMbhb4245_z7CBMgAOWJHgALDqD95kN301sM6JZrx3U9yx3sW1R8eKWCTyCTL0wWjWEMvxfb6rNMj5C9Pt2ployTF"
+          
+        },
+      });
+      console.log("Suriya not detected:", response.status);
+    }
+    
+  }, 10000); // 10 seconds
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video)
+      .withFaceLandmarks()
+      .withFaceDescriptors();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    resizedDetections.forEach((d) => {
+      const result = faceMatcher.findBestMatch(d.descriptor);
+      const box = d.detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: result.label,
+      });
+      if (result.label === "suriya") {
+        isSuriyaDetected = true;
+      }
       drawBox.draw(canvas);
     });
   }, 100);
